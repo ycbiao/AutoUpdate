7ac51dfdb17145e562c3a52dfd94b1df015374f5
ycbiao <394643582@qq.com>
2016-12-27 15:35:12 +0800

    ni

---
+++ app/src/main/java/com/ycb/autoupdate/UpdataInfo.java
@@ -0,0 +1,29 @@
+package com.ycb.autoupdate;
+
+/**
+ * Created by biao on 2016/12/27.
+ */
+
+public class UpdataInfo {
+    private String version;
+    private String url;
+    private String description;
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
